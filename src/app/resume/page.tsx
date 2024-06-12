c43a0b9adba47ee812fda0112b5296fca14bb4bb
Mohammed Armaan <armaan0243@gmail.com>
2024-06-12 21:01:38 +0530

    small change

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -5,7 +5,6 @@ const page = () => {
     const pdfUrl = '/resume.pdf';
   return (
     <>
-      
       <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
         <h1 className="text-3xl font-bold mb-4">Resume</h1>
         <iframe
